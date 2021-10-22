c863fbeda1163eeae8b79229b0a668a498b100b4
Lucas Brand <57752384+LVBrand@users.noreply.github.com>
2021-10-23 01:19:21 +0200

    J'ai changé d'avis
    On va utiliser un dataset de chiens et de chats venant de Kaggle, et non pas de loups et chiens (car je préfère et en plus il y a beaucoup plus de donnée dans celui de dogs_vs_cats)

--- apprentissage.py
+++ apprentissage.py
@@ -1,9 +1,18 @@
 # Tensorflow & Keras
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras import Model
 
 # Autres librairies
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+# Loading Dataset
+train_data = image_dataset_from_directory(
+    directory='.\data'
+    
+
+)
