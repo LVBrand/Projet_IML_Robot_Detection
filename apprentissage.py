b6e38eb2aedaf2d0c8dc6028a2f01244d3e21895
Lucas Brand <57752384+LVBrand@users.noreply.github.com>
2021-10-22 20:03:00 +0200

    dataset added

---
+++ apprentissage.py
@@ -0,0 +1,9 @@
+# Tensorflow & Keras
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+# Autres librairies
+import numpy as np
+import matplotlib.pyplot as plt
+
