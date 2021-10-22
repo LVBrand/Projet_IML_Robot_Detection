9b44cfb82262718b2e10a4cef5947c9e59810a87
Lucas Brand <57752384+LVBrand@users.noreply.github.com>
2021-10-22 19:00:45 +0200

    Initial commit

---
+++ main_robot.py
@@ -0,0 +1,26 @@
+# installer : qibullet, opencv-contrib-python (avec pip)
+from qibullet import SimulationManager
+from qibullet import PepperVirtual
+import pybullet as p
+import cv2
+import time
+
+
+if __name__ == "__main__":
+    simulation_manager = SimulationManager()
+    client_id = simulation_manager.launchSimulation(gui=True)
+
+    # Pepper, le robot raciste
+    pepper = simulation_manager.spawnPepper(
+        client_id,
+        spawn_ground_plane=True)
+
+
+    # Création d'une camera
+    handle = pepper.subscribeCamera(PepperVirtual.ID_CAMERA_TOP)
+
+    # Laser
+    pepper.showLaser(True)
+    pepper.subscribeLaser()
+
+    
