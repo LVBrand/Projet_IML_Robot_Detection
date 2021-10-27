7679c2b0c8b8ddf1cd565b847428708ffcf0cb5f
Lucas Brand <luckane@technifree.fr>
2021-10-28 00:05:20 +0200

    Added comments

--- main_robot.py
+++ main_robot.py
@@ -12,6 +12,8 @@ import numpy as np
 
 # Chemin vers le dataset de Test de kaggle (à changer suivant l'utilisateur)
 kaggleTestDatasetPath = '/Users/Lucas/Documents/Cours/S9 - SIIA/IML - Interactive Machine Learning/Projet_IML_Robot/kaggle_dataset_dogs_vs_cats_uncompressed/test'
+
+# On déclare ces deux booléens
 predState = False
 imgIsThere = False
 
@@ -40,31 +42,29 @@ if __name__ == "__main__":
 
 
     # Fonction de prédiction
+    # On resize l'image et on rajoute une dimension pour avoir une donnée conforme au layer
+    # d'entrée : [1, 150, 150, 3]
     def predict_animal(img, model):    
         categories = ['Chat', 'Chien']
-        #img = image.load_img(img_path, target_size=(150,150))
         x = cv2.resize(img, (150,150), interpolation = cv2.INTER_AREA)
-        #x = np.asarray(img)
         x = image.img_to_array(x)
         x = np.expand_dims(x, axis=0)
-        #x = np.concatenate((x, 3), axis=None)
-        #x = x * (1./255)
         pred = categories[int(model.predict(x)[0][0])]
         return pred
 
     # On définit une fonction qui instancie un objet affichant une image à faire prédire
+    # On commence par instancier un mesh à partir de l'objet quad.obj
+    # Ensuite on charge la texture (ici, l'image qu'on veut faire prédire)
+    # Puis, on crée l'objet en lui donnant une position, une orientation et une forme.
+    # Enfin, on applique la texture à l'objet (On affiche l'image sur l'objet qui sert ici de tableau)
     def create_picture(position, orientation, new_img_path):
-        # On instancie un mesh à partir de l'objet quad.obj
         visualShapeId = p.createVisualShape(p.GEOM_MESH, fileName='./obj/quad.obj')
-        # On charge la texture (ici, l'image qu'on veut faire prédire)
         textureUniqueId = p.loadTexture(new_img_path)
-        # On crée l'objet en lui donnant une position, une orientation et une forme
         multiBodyId = p.createMultiBody(
             baseVisualShapeIndex=visualShapeId,
             basePosition=position,
             baseOrientation=p.getQuaternionFromEuler(orientation))
-        # On applique la texture à l'objet (On affiche l'image sur l'objet qui sert ici de tableau)
-        p.changeVisualShape(multiBodyId, -1, textureUniqueId=textureUniqueId)
+         p.changeVisualShape(multiBodyId, -1, textureUniqueId=textureUniqueId)
         return multiBodyId
 
     """ 
@@ -79,6 +79,8 @@ if __name__ == "__main__":
             cv2.waitKey(1)
 
             # On appuie sur la touche C pour changer d'image aléatoirement
+            # Pour ne pas avoir plusieurs images superposées, on vérifie qu'il n'existe pas déjà
+            # Un objet de créé, sinon on le supprime avant d'en créer un nouveau.
             if imgIsThere==False :
                 if keyboard.is_pressed('c'):
                     if predState==False:
@@ -86,7 +88,7 @@ if __name__ == "__main__":
                         print("Vous avez demandé une image. Veuillez patienter.")
                         random_file = random.choice(os.listdir(kaggleTestDatasetPath))
                         newTextureId = kaggleTestDatasetPath+'/'+random_file
-                        img = create_picture(position=[2,0,1], orientation=[0,0,0], new_img_path=newTextureId)
+                        img = create_picture(position=[2.5,0,1], orientation=[0,0,0], new_img_path=newTextureId)
                         print("L'image est arrivée saine et sauve. Bisous.")
                         imgIsThere = True
             else:
@@ -98,20 +100,20 @@ if __name__ == "__main__":
                         print("Vous avez demandé une nouvelle image. Veuillez patienter.")
                         random_file = random.choice(os.listdir(kaggleTestDatasetPath))
                         newTextureId = kaggleTestDatasetPath+'/'+random_file
-                        img = create_picture(position=[2,0,1], orientation=[0,0,0], new_img_path=newTextureId)
+                        img = create_picture(position=[2.5,0,1], orientation=[0,0,0], new_img_path=newTextureId)
                         print("La nouvelle image est arrivée saine et sauve. Re-bisous.")
                         imgIsThere = True
 
-                    
+            # On appuie sur X pour débuter la prédiction       
             if keyboard.is_pressed('x'):
                 predState = True
-                print("Début de la prédiction")
+                print("Début de la prédiction...")
                 a = predict_animal(capture, model)
                 print("Pepper a prédit : ", a)
                 # p.removeBody(img)
                 # print("img removed")
                 predState = False
-                print("Arrêt de la prédiction")
+                print("Fin de la prédiction.")
                 
             
     except KeyboardInterrupt:
